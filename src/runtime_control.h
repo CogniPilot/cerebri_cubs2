751a68475cc63f5cad66366d2d42d9594e41609a
James Goppert <james.goppert@gmail.com>
2026-07-11 22:23:07 -0400

    Support realtime and lockstep FastDyn modes
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- src/runtime_control.h
+++ src/runtime_control.h
@@ -9,7 +9,20 @@
 /* Register Synapse ParamSet/Get and TrajectorySet/Get services.  Incoming
  * changes are staged by the Zenoh thread and applied atomically by
  * cubs2_runtime_control_apply() on the 50 Hz controller thread. */
+#if defined(CONFIG_CUBS2_RUNTIME_CONTROL)
 bool cubs2_runtime_control_init(FixedWingOuterLoopState *model);
 void cubs2_runtime_control_apply(FixedWingOuterLoopState *model, bool armed);
+#else
+static inline bool cubs2_runtime_control_init(FixedWingOuterLoopState *model) {
+  (void)model;
+  return true;
+}
+
+static inline void cubs2_runtime_control_apply(FixedWingOuterLoopState *model,
+                                               bool armed) {
+  (void)model;
+  (void)armed;
+}
+#endif
 
 #endif
